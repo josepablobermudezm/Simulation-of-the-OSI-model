3d3c530828c693f6323ce50cafdad93f02a60088
JordiRodriguezC <ycorrales120@gmail.com>
2020-05-30 21:05:53 -0600

    :boom:

--- Clases/enlace.py
+++ Clases/enlace.py
@@ -8,8 +8,6 @@ class enlace:
         result = ""
         for c in mensaje:
             bits = bin(ord(c))[2:]
-            print("Letra: "+c)
-            print("Binario: " + bits)
             bits = '00000000'[len(bits):] + bits
             result+=bits
             #result.extend([int(b) for b in bits])
